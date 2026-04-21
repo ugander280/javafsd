53a32b8adb7807a0001693c66f6ff2306975d1cd
Administrator <Administrator@DESKTOP-15TFTUR>
2026-04-21 08:49:34 +0530

    day1

---
+++ Test/src/test/java/day1/details.java
@@ -0,0 +1,14 @@
+package day1;  // Make sure this file is inside the 'day1' folder in your project
+
+public class details {
+    public static void main(String[] args) {
+        System.out.println("Person Details:");
+        System.out.println("_____________________"); // 21 underscores to match sample
+        System.out.println();
+        System.out.println("First Name: Divya");
+        System.out.println("Last Name: Bharathi");
+        System.out.println("Gender: F");
+        System.out.println("Age: 20");
+        System.out.println("Weight: 85.55");
+    }
+}
